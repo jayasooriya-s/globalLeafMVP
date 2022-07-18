2c9f30231cfbb42e20cb27ce897051eb0e6f6331
jayasooriya <sjsooriya96@gmail.com>
2022-07-18 22:37:56 +0530

    command updated

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -4,18 +4,18 @@ import { render } from '@testing-library/react'
 import Home, { getBranchData } from '../pages/index'
 import formatNumber from '../helper/formatNumber'
 import '@testing-library/jest-dom'
-import { act } from 'react-dom/test-utils'
+
 
 describe('Home', () => {
 
-    it('renders a app', () => {
+    test('renders a app', () => {
         render(<Home />)
     }),
 
-        it('formatting Sale Amount with currency', () => {
+        test('formatting Sale Amount with currency', () => {
             expect(formatNumber(465)).toBe('465.00 $')
         }),
-        it('formatting sale with currency and comma in 1000 s', () => {
+        test('formatting sale with currency and comma in 1000 s', () => {
             expect(formatNumber(1000)).toBe('1,000.00 $')
         }),
 
